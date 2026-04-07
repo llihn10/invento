62f5839520e110bec4a403ba12efa24f2c85d73f
llihn10 <nblinggg10@gmail.com>
2026-04-07 17:41:28 +0700

    feat: add sidebar

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import Sidebar from "@/components/sidebar";
+
+
+export default function DashboardPage() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <Sidebar currentPath="/dashboard" />
+            <main className="ml-64 p-8">
+                {/* Header */}
+                <div className="mb-8">
+                    <div className="flex items-center justify-between">
+                        <div>
+                            <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                            <p className="text-sm text-gray-500">Welcome back! Here is an overview of your inventory</p>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Key metrics */}
+                <div></div>
+            </main>
+        </div>
+    )
+}
---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@/app/generated/prisma";
+const prisma = new PrismaClient();
+
+async function main() {
+    const demoUserId = "99ef8d3a-7716-4874-a66d-434efc7b2429";
+
+    await prisma.product.createMany({
+        data: Array.from({ length: 25 }).map((_, i) => ({
+            userId: demoUserId,
+            name: `Product ${i + 1}`,
+            price: (Math.random() * 90 + 10).toFixed(2),
+            quantity: Math.floor(Math.random() * 20),
+            lowStockAt: 5,
+            createdAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * (i * 5)),
+        }))
+    })
+
+    console.log("Seed data created successfully!")
+    console.log(`Created 25 products for user id: ${demoUserId}`)
+}
+
+main()
+    .catch((e) => {
+        console.error(e)
+        process.exit(1)
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
